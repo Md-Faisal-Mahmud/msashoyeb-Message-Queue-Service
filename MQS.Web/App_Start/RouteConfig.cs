dfc07fd5827745285df4379c0fac8c0af1cebcf1
msashoyeb <msashoyeb@gmail.com>
2024-01-17 18:39:31 +0600

    Removed unnecessary files

--- MQS.Web/App_Start/RouteConfig.cs
+++ MQS.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MQS.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "MessageQueue", action = "Dashboard", id = UrlParameter.Optional }
             );
         }
     }
