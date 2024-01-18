7f45337938e1a6a1fd6347105f3d554134f88359
msashoyeb <msashoyeb@gmail.com>
2024-01-18 14:09:26 +0600

    Action added into controller

--- MQS.Web/Controllers/MessageQueueController.cs
+++ MQS.Web/Controllers/MessageQueueController.cs
@@ -1,16 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.Ajax.Utilities;
+using MQS.Application.Services;
+using MQS.Infrastructure.Data;
+using MQS.Infrastructure.Services;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace MQS.Web.Controllers
 {
     public class MessageQueueController : Controller
     {
+        MessageQueueService _messageQueueService = new MessageQueueService();
+        //    private readonly IMessageQueueService _messageQueueService;
+        //    public MessageQueueController() { }
+        //    public MessageQueueController(IMessageQueueService messageQueueService)
+        //    {
+        //        _messageQueueService = messageQueueService;
+        //    }
+
+        [HttpGet]
         public ActionResult Dashboard()
         {
+            ViewBag.LogMessages = LogMessages.GetMessages();
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult StartQueueProcessing()
+        {
+            Task.Run(() => _messageQueueService.ProcessMessages());
+            return RedirectToAction("Dashboard");
+        }
+
+        public ActionResult GetLogMessages()
+        {
+            var logMessages = LogMessages.GetMessages();
+            return PartialView("_LogMessagesPartial", logMessages);
         }
     }
 }
