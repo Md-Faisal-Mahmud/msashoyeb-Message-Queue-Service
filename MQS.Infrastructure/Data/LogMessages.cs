9d0b855952c0b283f32937aa0ed262fbfe4f2a5b
msashoyeb <msashoyeb@gmail.com>
2024-01-18 14:17:45 +0600

    LogMessages added

---
+++ MQS.Infrastructure/Data/LogMessages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQS.Infrastructure.Data
+{
+    public static class LogMessages
+    {
+        private static readonly List<string> messages = new List<string>();
+        private static readonly object lockObject = new object();
+
+        public static void AddMessage(string message)
+        {
+            lock (lockObject)
+            {
+                messages.Add(message);
+            }
+        }
+
+        public static IEnumerable<string> GetMessages()
+        {
+            lock (lockObject)
+            {
+                return messages.ToList();
+            }
+        }
+    }
+}
