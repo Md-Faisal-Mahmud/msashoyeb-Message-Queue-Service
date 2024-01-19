e3000e540c355255fa2bf3cd2dd5686f3390a9aa
msashoyeb <msashoyeb@gmail.com>
2024-01-19 20:18:36 +0600

    Dependency applied

---
+++ MQS.Application/Utilities/IMessageQueueUtility.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQS.Application.Utilities
+{
+    public interface IMessageQueueUtility
+    {
+        void SendMessage(IModel channel, string exchange, string routingKey, string message);
+        void ConsumeMessages(IModel channel, string queueName, string prefix);
+        void SharedConsumeMessages(IModel orderChannel, string orderQueue, IModel inventoryChannel,
+            string inventoryQueue, int sharedConsumerNumber);
+    }
+}
