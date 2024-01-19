6c65a5ec3c645fb528ef4662554f875faa05823c
msashoyeb <msashoyeb@gmail.com>
2024-01-19 20:17:30 +0600

    Dependency setup

---
+++ MQS.Web/App_Start/NinjectResolver.cs
@@ -0,0 +1,39 @@
+﻿using MQS.Application.Services;
+using MQS.Application.Utilities;
+using MQS.Infrastructure.Services;
+using MQS.Infrastructure.Utilities;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MQS.Web.App_Start
+{
+    public class NinjectResolver : System.Web.Mvc.IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+
+        public NinjectResolver()
+        {
+            _kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            this._kernel.Bind<IMessageQueueService>().To<MessageQueueService>();
+            this._kernel.Bind<IMessageQueueUtility>().To<MessageQueueUtility>();
+        }
+    }
+}
--- MQS.Web/Global.asax.cs
+++ MQS.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MQS.Web.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,7 @@ namespace MQS.Web
     {
         protected void Application_Start()
         {
+            DependencyResolver.SetResolver(new NinjectResolver());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
